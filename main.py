8bc92b02f98ab26b1d1deec4a55d062cbc322fc9
Gabriel Olvera <gavoxolvera@gmail.com>
2021-04-05 04:07:33 -0500

    Sets Operations
    DOne

--- main.py
+++ main.py
@@ -2,9 +2,10 @@ from tkinter import *
 import tkinter as tk
 import ExpressionSplitter
 import ExpressionsProcessing
+import sets_engine
 
 # Tkinter Window Nav Configuration
-
+decition = 0
 window = Tk()
 window.title("FCC ToolKit")
 window.geometry("1500x700")
@@ -12,21 +13,72 @@ window.geometry("1500x700")
 window.rowconfigure(0, weight=1)
 window.columnconfigure(0, weight=1)
 
+Frame0 = tk.Frame(window)
 Frame1 = tk.Frame(window)
 Frame2 = tk.Frame(window)
+Frame3 = tk.Frame(window)
 
 logicalexpression = tk.StringVar()
+optionSets = tk.StringVar()
+Seta = tk.StringVar()
+Setb= tk.StringVar()
+Setc = tk.StringVar()
+Choice2= tk.StringVar()
 
+
+canvas0 = Canvas(Frame0, width=1472, height=730)
 canvas1 = Canvas(Frame1, width=1472, height=730)
 canvas2 = Canvas(Frame2, width=1472, height=730)
+canvas3 = Canvas(Frame3, width=1472, height=730)
 background = PhotoImage(file='bg1check.ppm')
 background2 = PhotoImage(file='bg2check.ppm')
+canvas0.pack()
 canvas1.pack()
 canvas2.pack()
+canvas3.pack()
 
+def goToOps():
+    Frame3.tkraise()
+
+def goToTruths():
+    Frame1.tkraise()
 
 def show_frame(frame):
     frame.tkraise()
+
+def checkSubmitSet():
+    decition = optionSets.get()
+    set_a = Seta.get()
+    set_b = Setb.get()
+    set_c = Setc.get()
+    FFirst, SSecond, TThird, FFourth = sets_engine.get_operative_set(decition,set_a,set_b,set_c)
+
+
+    canvas3_label_First["text"] = FFirst
+    canvas3_label_Second["text"] = SSecond
+    canvas3_label_Third["text"] = TThird
+    canvas3_label_Fourth["text"] = FFourth
+
+    canvas3_Btn["state"] = "normal"
+    canvas3_Btn["height"] = 2
+    canvas3_Btn["width"] = 10
+
+    canvas3_entry_choice2["width"] = 30
+
+def continueProcedure():
+    option = Choice2.get()
+    decition = optionSets.get()
+    set_a = Seta.get()
+    set_b = Setb.get()
+    set_c = Setc.get()
+    requested = sets_engine.get_requested_union(decition,option,set_a,set_b,set_c)
+    print("this is requested", requested)
+    canvas3_requested["text"] = requested
+
+
+
+
+
 
 
 def use_logical_expression(frame, canvas):
@@ -70,12 +122,33 @@ def createTable(final_expression, full_dict, canvas):
     canvas.create_window(100, 100, anchor=NW, window=canvasTable)
 
 
-for frame in (Frame1, Frame2):
+for frame in (Frame0, Frame1, Frame2, Frame3):
     frame.grid(row=0,column=0,sticky='nsew')
+
+# Canvas 0 Code 
+
+canvas0_title= tk.Label(canvas0, text="Welcome to FCC ToolKit", font= "times 35")
+canvas0_title.pack()
+canvas0_title.place(relx=.33, rely=.1)
+
+canvas0_Label =  tk.Label(canvas0, text="What Operation do you want?", font ="times 35")
+canvas0_Label.pack()
+canvas0_Label.place(relx=.30, rely=.2)
+
+canvas0_Btn = tk.Button(canvas0, height=20, width= 30, text="Sets Operations", command=lambda: goToOps())
+canvas0_Btn.pack(ipady=15)
+canvas0_Btn.place(relx=.30, rely=.45)
+canvas0_BtnTruth = tk.Button(canvas0, height= 20, width =30 , text= "Truth Tables", command=lambda: goToTruths())
+canvas0_BtnTruth.pack(ipady=15)
+canvas0_BtnTruth.place(relx=.55, rely=.45)
+canvas0.create_image(0, 0, anchor=NW, image=background)
+
+canvas0.pack(fill='both', expand=True)
+
 
 # Canvas 1 Code 
 
-canvas1_title = tk.Label(canvas1, text="Welcome to FCC ToolKit", font= "times 35")
+canvas1_title = tk.Label(canvas1, text="Truth Table Generator", font= "times 35")
 canvas1_title.pack()
 canvas1_title.place(relx=.33, rely=.1)
 
@@ -106,7 +179,7 @@ canvas2_title = tk.Label(canvas2, text="Truth Table: ", font="times 35")
 canvas2_title.pack()
 canvas2_title.place(relx=.05, rely=.05)
 
-canvas2_btn = tk.Button(canvas2, height=3, width=20, text="Back to Main Menu ", command=lambda: show_frame(Frame1))
+canvas2_btn = tk.Button(canvas2, height=3, width=20, text="Back to Main Menu ", command=lambda: show_frame(Frame0))
 canvas2_btn.pack()
 canvas2_btn.place(relx= .8 , rely= .9)
 
@@ -114,6 +187,103 @@ canvas2_btn.place(relx= .8 , rely= .9)
 canvas2.create_image(0, 0, anchor=NW, image=background2)
 canvas2.pack(fill='both', expand=True)
 
+# Canvas 3 Code 
+canvas3_title = tk.Label(canvas3, text="Sets Operations", font= "times 35")
+canvas3_title.pack()
+canvas3_title.place(relx=.40, rely=.05)
 
-show_frame(Frame1)
+canvas3_entry = tk.Entry(canvas3,width=24, textvariable=optionSets)
+canvas3_entry.pack()
+canvas3_entry.place(relx=.28, rely=.5)
+
+canvas3_label_a = tk.Label(canvas3,font = "times 30", text= "Operations")
+canvas3_label_a.pack()
+canvas3_label_a.place(relx= .1 , rely=.15)
+
+canvas3_label_b = tk.Label(canvas3,font = "times 20", text= "Choice : ")
+canvas3_label_b.pack()
+canvas3_label_b.place(relx= .17 , rely=.48)
+
+canvas3_label_seta = tk.Label(canvas3,font = "times 20", text= "Set A: ")
+canvas3_label_setb = tk.Label(canvas3,font = "times 20", text= "Set B: ")
+canvas3_label_setc = tk.Label(canvas3,font = "times 20", text= "Set C: ")
+
+canvas3_label_seta.pack()
+canvas3_label_setb.pack()
+canvas3_label_setc.pack()
+
+canvas3_label_seta.place(relx= .17, rely= .60)
+canvas3_label_setb.place(relx= .17, rely= .65)
+canvas3_label_setc.place(relx= .17, rely= .70)
+
+canvas3_entry_seta = tk.Entry(canvas3, width = 30 , textvariable = Seta)
+canvas3_entry_setb = tk.Entry(canvas3, width = 30 , textvariable = Setb)
+canvas3_entry_setc = tk.Entry(canvas3, width = 30 , textvariable = Setc)
+
+canvas3_entry_seta.pack()
+canvas3_entry_setb.pack()
+canvas3_entry_setc.pack()
+
+canvas3_entry_seta.place(relx= .25, rely= .62)
+canvas3_entry_setb.place(relx= .25, rely= .67)
+canvas3_entry_setc.place(relx= .25, rely= .72)
+
+canvas3_buttonsubmit = tk.Button(canvas3, height=2, width=10,text="Submit",command=lambda: checkSubmitSet())
+canvas3_buttonsubmit.pack(ipady=10)
+canvas3_buttonsubmit.place(relx=.41, rely=.66)
+
+canvas3_label_1 = tk.Label(canvas3,font = "times 20", text= "1- Union")
+canvas3_label_2 = tk.Label(canvas3,font = "times 20", text= "2- Intersection")
+canvas3_label_3 = tk.Label(canvas3,font = "times 20", text= "3- Diference")
+canvas3_label_4 = tk.Label(canvas3,font = "times 20", text= "4- Symmetric Diference ")
+
+canvas3_label_1.pack()
+canvas3_label_2.pack()
+canvas3_label_3.pack()
+canvas3_label_4.pack()
+
+canvas3_label_1.place(relx = .1, rely= .22)
+canvas3_label_2.place(relx = .1, rely= .26) 
+canvas3_label_3.place(relx = .1, rely= .30)
+canvas3_label_4.place(relx = .1, rely= .34)
+
+canvas3_label_First= tk.Label(canvas3, text="", font= "times 20")
+canvas3_label_First.pack()
+canvas3_label_First.place(relx=.7, rely=.22)
+
+canvas3_label_Second= tk.Label(canvas3, text="", font= "times 20")
+canvas3_label_Second.pack()
+canvas3_label_Second.place(relx=.7, rely=.32)
+
+canvas3_label_Third= tk.Label(canvas3, text="", font= "times 20")
+canvas3_label_Third.pack()
+canvas3_label_Third.place(relx=.7, rely=.42)
+
+canvas3_label_Fourth= tk.Label(canvas3, text="", font= "times 20")
+canvas3_label_Fourth.pack()
+canvas3_label_Fourth.place(relx=.7, rely=.52)
+
+canvas3_entry_choice2 = tk.Entry(canvas3, width=0, textvariable=Choice2)
+canvas3_entry_choice2.pack()
+canvas3_entry_choice2.place(relx=.7,rely=.58)
+
+canvas3_Btn= tk.Button(canvas3, height=0, width=0,text="Submit",command=lambda:continueProcedure())
+canvas3_Btn.pack(ipady=10)
+canvas3_Btn.place(relx=.7,rely=.64)
+canvas3_buttonsubmit = tk.Button(canvas3, height=2, width=10,text="Submit",command=lambda: checkSubmitSet())
+canvas3_buttonsubmit.pack(ipady=10)
+canvas3_buttonsubmit.place(relx=.41, rely=.66)
+
+canvas3_requested = tk.Label(canvas3, text="", font= "times 20")
+canvas3_requested.pack()
+canvas3_requested.place(relx=.7, rely=.7)
+
+canvas3_bbtn = tk.Button(canvas3, height=3, width=20, text="Back to Main Menu ", command=lambda: show_frame(Frame0))
+canvas3_bbtn.pack()
+canvas3_bbtn.place(relx= .8 , rely= .9)
+
+canvas3.create_image(0, 0, anchor=NW, image=background2)
+canvas3.pack(fill='both', expand=True)
+
+show_frame(Frame0)
 window.mainloop()
--- sets_engine.py
+++ sets_engine.py
@@ -1,25 +1,17 @@
-def get_sets():
-    input_set_a = input("Set A: ")
-    input_set_b = input("Set B: ")
-    input_set_c = input("Set C: ")
+def get_sets(seta,setb,setc):
+    #inputs are obtained in TK inter graphical display
+    input_set_a = seta
+    input_set_b = setb
+    input_set_c = setc
     set_a = input_set_a.split(",")
     set_b = input_set_b.split(",")
     set_c = input_set_c.split(",")
-    print(set_a, set_b, set_c)
+    
     return set_a, set_b, set_c
 
-
-def get_operative_set():
-    operation = input("Operación a realizar: \n1.- Unión\n2.- Intersección"
-                      "\n3.- Diferencia\n4.- Diferencia simétrica\n &>")
-    set_a, set_b, set_c = get_sets()
-    requested_union = []
+def get_requested_union(operation,option,seta,setb,setc):
+    set_a, set_b, set_c = get_sets(seta,setb,setc)
     if int(operation) == 1:
-        print("1.- A U B")
-        print("2.- B U C")
-        print("3.- A U C")
-        print("4.- A U (B U C)")
-        option = input("Conjunto: ")
         if int(option) == 1:
             requested_union = get_union(set_a, set_b)
         elif int(option) == 2:
@@ -27,13 +19,8 @@ def get_operative_set():
         elif int(option) == 3:
             requested_union = get_union(set_a, set_c)
         elif int(option) == 4:
-            requested_union = get_union(set_a, get_union(set_b, set_c))
+            requested_union = get_union(set_a, get_union(set_b, set_c)) 
     elif int(operation) == 2:
-        print("1.- A INT B")
-        print("2.- B INT C")
-        print("3.- A INT C")
-        print("4.- A INT (B INT C)")
-        option = input("Conjunto: ")
         if int(option) == 1:
             requested_union = get_intersection(set_a, set_b)
         elif int(option) == 2:
@@ -43,10 +30,6 @@ def get_operative_set():
         elif int(option) == 4:
             requested_union = get_intersection(set_a, get_intersection(set_b, set_c))
     elif int(operation) == 3:
-        print("1.- A - B")
-        print("2.- B - C")
-        print("3.- A - C")
-        option = input("Conjunto: ")
         if int(option) == 1:
             requested_union = get_difference(set_a, set_b)
         elif int(option) == 2:
@@ -54,21 +37,57 @@ def get_operative_set():
         elif int(option) == 3:
             requested_union = get_difference(set_a, set_c)
     elif int(operation) == 4:
-        print("1.- A delta B")
-        print("2.- B delta C")
-        print("3.- A delta B")
-        option = input("Conjunto: ")
         if int(option) == 1:
             requested_union = get_symmetric_difference(set_a, set_b)
         elif int(option) == 2:
             requested_union = get_symmetric_difference(set_b, set_c)
         elif int(option) == 3:
-            requested_union = get_symmetric_difference(set_a, set_c)
+            requested_union = get_symmetric_difference(set_a, set_c) 
+    print("Requested Main UNin ", requested_union)
     return requested_union
 
 
+    
+    
+
+
+def get_operative_set(operation,seta,setb,setc):
+    #operation is obtained in TK inter graphical display
+    """ operation = input("Operación a realizar: \n1.- Unión\n2.- Intersección"
+                      "\n3.- Diferencia\n4.- Diferencia simétrica\n &>") """
+    set_a, set_b, set_c = get_sets(seta,setb,setc)
+    if int(operation) == 1:
+        first_possible = "1.- A U B"
+        second_possible = "2.- B U C"
+        third_possible = "3.- A U C"
+        fourth_possible = "4.- A U (B U C)"
+
+        return first_possible, second_possible, third_possible, fourth_possible
+    elif int(operation) == 2:
+        first_possible = "1.- A INT B"
+        second_possible = "2.- B INT C"
+        third_possible = "3.- A INT C"
+        fourth_possible = "4.- A INT (B INT C)"
+
+        return first_possible, second_possible, third_possible, fourth_possible
+    elif int(operation) == 3:
+        first_possible = "1.- A - B"
+        second_possible = "2.- B - C"
+        third_possible = "3.- A - C"
+        fourth_possible = " "
+
+        return  first_possible, second_possible, third_possible, fourth_possible
+    elif int(operation) == 4:
+        first_possible = "1.- A delta B"
+        second_possible = "2.- B delta C"
+        third_possible = "3.- A delta C"
+        fourth_possible = " "
+
+        return  first_possible, second_possible, third_possible, fourth_possible
+    
+
+
 def get_union(set_1, set_2):
-    print(set_1, "U", set_2)
     union_array = set_1
     for i in set_2:
         if i not in set_1:
@@ -78,7 +97,7 @@ def get_union(set_1, set_2):
 
 def get_intersection(set_1, set_2):
     union_array = []
-    print(set_1, "INT", set_2)
+
     for i in set_1:
         for j in set_2:
             if i == j:
